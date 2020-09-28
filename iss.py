57f703198efef6c8b4fca4279ff77176f2506563
Robert Bolling <robboles1014@gmail.com>
2020-09-28 13:38:07 -0400

    Completed Part A

--- iss.py
+++ iss.py
@@ -1,9 +1,25 @@
 #!/usr/bin/env python
 
-__author__ = '???'
+__author__ = 'r-bolling with help from Kenzie Academy Lessons'
+
+import requests
+
+def get_astronauts():
+    r = requests.get('http://api.open-notify.org/astros.json')
+    r = r.json()
+    print(
+        f'There are {r["number"]} astronaut(s) in space'
+    )
+    for i in range(0, r["number"]):
+        print(
+            f'Name: {r["people"][i]["name"]}, '
+            f'Craft: {r["people"][i]["craft"]}'
+        )
+    return r
 
 
 def main():
+    get_astronauts()
     pass
 
 
